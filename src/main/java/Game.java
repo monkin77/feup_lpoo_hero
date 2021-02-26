df1f81d37099fd97c31dabdb9d10b2a5866a6a72
monkin77 <monkinsmurf@gmail.com>
2021-02-26 15:14:28 +0000

    Done task 7

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,6 +11,7 @@ import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10, y = 10;
 
     public Game(){
         try {
@@ -25,13 +28,45 @@ public class Game {
         }
     }
 
+    private void processKey(com.googlecode.lanterna.input.KeyStroke key) throws IOException {
+        System.out.println(key);
+
+        switch(key.getKeyType()){
+            case ArrowUp:
+                this.y--;
+                break;
+            case ArrowDown:
+                this.y++;
+                break;
+            case ArrowLeft:
+                this.x--;
+                break;
+            case ArrowRight:
+                this.x++;
+                break;
+        }
+
+        if(key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
+            this.screen.close();
+        }
+    }
+
     private void draw() throws IOException{
         this.screen.clear();
-        this.screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        this.screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         this.screen.refresh();
     }
 
     public void run() throws IOException {
-        draw();
+        while(true){
+            draw();
+
+            KeyStroke key = screen.readInput();
+
+            if(key.getKeyType() == KeyType.EOF)         // if the screen was closed
+                break;
+
+            processKey(key);
+        }
     }
 }
